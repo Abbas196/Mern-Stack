9d29f7371a9961d884a9ccdea190ecb5e6fad715
Abbas196 <55959324+Abbas196@users.noreply.github.com>
2022-05-08 10:17:52 -0500

    sescond commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log("hello word")
+const express = require('express')
+const dotenv = require('dotenv').config()
+// process.env allow us to access environment variables from .env file.
+const port = process.env.PORT || 5000
+
+// initialize our application 
+const app = express()
+app.listen(port, () => console.log(`Server started on port ${port}`))
